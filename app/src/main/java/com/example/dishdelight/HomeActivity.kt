ebea06afe79f4dac963993fc9e6b377626e77d0c
Sayed Haneef <sayedhaneef31@gmail.com>
2025-06-03 16:48:29 +0530

    Initail home screen done

--- app/src/main/java/com/example/dishdelight/HomeActivity.kt
+++ app/src/main/java/com/example/dishdelight/HomeActivity.kt
@@ -1,20 +1,72 @@
 package com.example.dishdelight
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.dishdelight.databinding.ActivityHomeBinding
+import com.example.dishdelight.databinding.PopularFoodCardBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        popluateFamousFood()
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    private fun popluateFamousFood() {
+        repeat(3) {
+            val popularFoodCardBinding = PopularFoodCardBinding.inflate(layoutInflater)
+
+            popularFoodCardBinding.foodNameIddddd.text="Green Salad"
+            popularFoodCardBinding.foodRatingIddddd.text="4.1 ⭐"
+            popularFoodCardBinding.foodPriceIddddd.text="₹ 349"
+
+            popularFoodCardBinding.btnAddIddddddd.setOnClickListener {
+                popularFoodCardBinding.quantitySelector.visibility = View.VISIBLE
+                popularFoodCardBinding.btnAddIddddddd.visibility = View.GONE
+                popularFoodCardBinding.quantityIddddd.text="1"
+            }
+
+            popularFoodCardBinding.btnPlusIddddd.setOnClickListener {
+                var quantity = popularFoodCardBinding.quantityIddddd.text.toString().toInt()
+                quantity++
+                popularFoodCardBinding.quantityIddddd.text = quantity.toString()
+            }
+
+            popularFoodCardBinding.btnMinusIdddd.setOnClickListener {
+                var quantity = popularFoodCardBinding.quantityIddddd.text.toString().toInt()
+                if(quantity>1){
+                    quantity--
+                    popularFoodCardBinding.quantityIddddd.text = quantity.toString()
+                }
+                else
+                {
+                    popularFoodCardBinding.quantitySelector.visibility = View.GONE
+                    popularFoodCardBinding.btnAddIddddddd.visibility = View.VISIBLE
+
+                }
+            }
+
+            //Adding card view to container
+            binding.containerPopularFoods.addView(popularFoodCardBinding.root)
+
         }
     }
 }
